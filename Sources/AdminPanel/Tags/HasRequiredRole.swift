40f3c372a9310e1722ba8bd5036ea0672bd6583d
Paul <paultoffoloni@gmail.com>
2023-04-04 14:13:04 +0200

    Update required role tag

--- Sources/AdminPanel/Tags/HasRequiredRole.swift
+++ Sources/AdminPanel/Tags/HasRequiredRole.swift
@@ -1,24 +1,20 @@
 import Leaf
 
-public final class HasRequiredRole<U: AdminPanelUserType>: TagRenderer {
-    public func render(tag: TagContext) throws -> Future<TemplateData> {
-        try tag.requireParameterCount(1)
-        let request = try tag.requireRequest()
-        let container = try request.privateContainer.make(CurrentUserContainer<U>.self)
-
+public struct HasRequiredRole: LeafTag {
+    public func render(_ ctx: LeafContext) throws -> LeafData {
+        try ctx.requireParameterCount(1)
+        
         guard
-            let roleString: String = tag.parameters[0].string,
-            let requiredRole = U.Role(roleString)
+            let roleString = ctx.parameters[0].string,
+            let requiredRole = AdminPanelUser.Role(rawValue: roleString)
         else {
-            throw tag.error(reason: "Invalid role requirement")
+            throw "Invalid role requirement"
         }
-
-        guard
-            let userRole = container.user?.role
-        else {
-            return tag.future(.bool(false))
+        
+        guard let userRole = ctx.request?.auth.get(AdminPanelUser.self)?.role else {
+            return .bool(false)
         }
-
-        return tag.future(.bool(userRole >= requiredRole))
+        
+        return .bool(userRole >= requiredRole)
     }
 }
